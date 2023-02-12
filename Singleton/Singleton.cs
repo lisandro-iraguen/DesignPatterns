1d4ce012859c2e30f0528bb197e8fa5618fce6af
lisandro-iraguen <liraguen@est.frba.utn.edu.ar>
2023-02-12 14:49:22 -0300

    singleton pattern

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Singletons
+{
+
+    public sealed class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+        public void someBusinessLogic()
+        {
+            // ...
+        }
+    }
+}
+
