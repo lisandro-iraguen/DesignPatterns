d630a00c0a30f0acfbc5edac9f683c8a104eca1e
lisandro-iraguen <liraguen@est.frba.utn.edu.ar>
2023-02-13 16:26:40 -0300

    more design patterns

---
+++ Bridge/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Bridge;
+
+Client client = new Client();
+
+Abstraction abstraction;
+abstraction = new Abstraction(new ConcreteImplementationA());
+client.ClientCode(abstraction);
+
+Console.WriteLine();
+
+abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+client.ClientCode(abstraction);
