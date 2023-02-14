72508e73adec00f0c2bfea1bee75ac26614ca825
lisandro-iraguen <liraguen@est.frba.utn.edu.ar>
2023-02-14 18:40:52 -0300

    iterator pattern

---
+++ Iterator/IteratorAggregate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    public abstract class IteratorAggregate : IEnumerable
+    {
+        // Returns an Iterator or another IteratorAggregate for the implementing
+        // object.
+        public abstract IEnumerator GetEnumerator();
+    }
+
+}
