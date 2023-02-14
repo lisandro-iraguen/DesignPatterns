58490467f2fe37584b055296f895202a5538434d
lisandro-iraguen <liraguen@est.frba.utn.edu.ar>
2023-02-14 18:26:22 -0300

    visitor pattern

---
+++ Visitor/IVisitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    public interface IVisitor
+    {
+        void VisitConcreteComponentA(ConcreteComponentA element);
+
+        void VisitConcreteComponentB(ConcreteComponentB element);
+    }
+
+}
